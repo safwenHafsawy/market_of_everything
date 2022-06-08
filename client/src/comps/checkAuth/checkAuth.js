0fc7bb794a241f132e2a244346d6a45d7f1157df
Safwen Hafsawy <58145129+safwenHafsawy@users.noreply.github.com>
2022-06-08 15:55:15 +0100

    debugging

--- client/src/comps/checkAuth/checkAuth.js
+++ client/src/comps/checkAuth/checkAuth.js
@@ -1,28 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { loginAction } from "../../redux/actions";
 
 const CheckAuth = () => {
-  const dispatcher = useDispatch();
-  useEffect(() => {
-    fetch("/api/home", {
-      method: "GET",
-      headers: { "content-type": "application/json" },
-      credentials: "include",
-    })
-      .then((res) => {
-				console.log(res);
-        res
-          .json()
-          .then((data) => {
-						console.log(data)
-            if (data.userID) dispatcher(loginAction(data.userID,data.username, true));
-          })
-          .catch((e) => console.log(e));
-      })
-      .catch((e) => console.log(e));
-  }, []);
-  return null;
+	const dispatcher = useDispatch();
+	useEffect(async () => {
+		const response = await fetch("/api/home", {
+			method: "GET",
+			headers: { "content-type": "application/json" },
+			credentials: "include",
+		});
+		const response = await fetch("/api/home", {
+			method: "GET",
+			headers: { "content-type": "application/json" },
+			credentials: "include",
+		});
+
+		console.log(response);
+		const data = await response.json();
+		console.log(data);
+
+		if (data.userID) dispatcher(loginAction(data.userID, data.username, true));
+	}, []);
+	return null;
 };
 
 export default CheckAuth;
